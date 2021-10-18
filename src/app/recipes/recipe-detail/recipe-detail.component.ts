f06073dbdf88c3fe353f9730d89e31be20759902
Ulisses Hen <ulisseshen@gmail.com>
2021-10-18 20:46:30 -0300

    adicionado components recipes e shopping-list

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styles: [
+  ]
+})
+export class RecipeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
