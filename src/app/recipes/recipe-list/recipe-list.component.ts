496224af9013f01266ef851967de8dc69f4017ef
Ulisses Hen <ulisseshen@gmail.com>
2021-10-19 19:32:01 -0300

    criado a estrutura do lista de receitas

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Samduba","bom agora","https://assets.b9.com.br/wp-content/uploads/2021/07/subwayb9-1280x677.jpg"),
+    new Recipe("Samduba 2","faltoo","https://segredosdomundo.r7.com/wp-content/uploads/2015/04/destaque33.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
