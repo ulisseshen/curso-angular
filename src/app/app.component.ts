7c78d8e57cc9ee62263effe29cdd8953f1451612
Ulisses Hen <ulisseshen@gmail.com>
2021-10-20 13:57:51 -0300

    removido .github | adiciona navegação via ngIf a EventEmitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-complete-guide';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature:string){
+    this.loadedFeature = feature;
+  }
 }
