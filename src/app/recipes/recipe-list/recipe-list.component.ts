12f171a99df3268c8fc7ff4b8014f9dd430f8ee6
Ulisses Hen <ulisseshen@gmail.com>
2021-10-20 17:39:29 -0300

    dados passando via Event e property bind

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,17 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("Samduba","bom agora","https://assets.b9.com.br/wp-content/uploads/2021/07/subwayb9-1280x677.jpg"),
-    new Recipe("Samduba 2","faltoo","https://segredosdomundo.r7.com/wp-content/uploads/2015/04/destaque33.jpg")
+    new Recipe("Samduba", "bom agora", "https://assets.b9.com.br/wp-content/uploads/2021/07/subwayb9-1280x677.jpg"),
+    new Recipe("Samduba 2", "faltoo", "https://segredosdomundo.r7.com/wp-content/uploads/2015/04/destaque33.jpg")
   ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
