b9138a096e8b2f204faf480632161135cb45656c
Ulisses Hen <ulisseshen@gmail.com>
2021-11-01 20:39:25 -0300

    adicionado parametros simples da rota dos usuarios

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -6,8 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
@@ -16,6 +16,7 @@ import { RouterModule, Routes } from '@angular/router';
 const appRoutes: Routes = [
   { path:'' , component: HomeComponent},
   { path:'users' , component: UsersComponent},
+  { path:'users/:id/:name' , component: UserComponent},
   { path:'servers' , component: ServersComponent},
 ]
 @NgModule({
--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id : this.router.snapshot.params['id'],
+      name: this.router.snapshot.params['name']
+    }
   }
 
 }
