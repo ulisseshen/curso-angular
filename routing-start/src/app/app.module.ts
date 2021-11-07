d343fcc555b964ea6484d165e3bdfdd9625a2609
Ulisses Hen <ulisseshen@gmail.com>
2021-11-07 01:42:05 -0300

    adicionado router-outlet para rotas aninhadas, facilitndo a navegação dentro da mesma pagina

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -14,12 +14,18 @@ import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
-  { path:'' , component: HomeComponent},
-  { path:'users' , component: UsersComponent},
-  { path:'users/:id/:name' , component: UserComponent},
-  { path:'servers' , component: ServersComponent},
-  { path:'servers/:id' , component: ServerComponent},
-  { path:'servers/:id/edit' , component: EditServerComponent},
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent }
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  },
 ]
 @NgModule({
   declarations: [
