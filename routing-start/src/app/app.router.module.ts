89671243871894cf1a2c8693548826b29e959760
Ulisses Hen <ulisseshen@gmail.com>
2021-11-08 18:11:31 -0300

    implementado a AuthGuard para rotas filhas (ChildRoutes)

--- routing-start/src/app/app.router.module.ts
+++ routing-start/src/app/app.router.module.ts
@@ -17,7 +17,7 @@ const appRoutes: Routes = [
     ]
   },
   {
-    path: 'servers', canActivate: [AuthGuard], component: ServersComponent, children: [
+    path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
       { path: ':id', component: ServerComponent },
       { path: ':id/edit', component: EditServerComponent }
     ]
--- routing-start/src/app/auth-guard.service.ts
+++ routing-start/src/app/auth-guard.service.ts
@@ -2,28 +2,35 @@ import { Injectable } from "@angular/core";
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   Router,
   RouterStateSnapshot,
-  UrlTree } from "@angular/router";
+  UrlTree
+} from "@angular/router";
 import { Observable } from "rxjs/Observable";
 import { AuthService } from "./auth.service";
 
-@Injectable({providedIn:'root'})
-export class AuthGuard implements CanActivate {
+@Injectable({ providedIn: 'root' })
+export class AuthGuard implements CanActivate, CanActivateChild {
 
-  constructor(private authService:AuthService,
-    private router: Router){}
+  constructor(private authService: AuthService,
+    private router: Router) { }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.canActivate(childRoute, state);
+  }
 
   canActivate(route: ActivatedRouteSnapshot,
-              state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
     return this.authService.isAuthenticated()
-    .then(
-      (authenticated:boolean) =>{
-        if(authenticated){
-          return true;
+      .then(
+        (authenticated: boolean) => {
+          if (authenticated) {
+            return true;
+          }
+          this.router.navigate(['/'])
         }
-        this.router.navigate(['/'])
-      }
-    )
+      )
   }
 }
