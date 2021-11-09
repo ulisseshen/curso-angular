c7165e345d88b0d1fc0c5d36cbe52841d0036567
Ulisses Hen <ulisseshen@gmail.com>
2021-11-09 13:10:16 -0300

    adicionar Resolver para injetar dados baseado na rota

--- routing-start/src/app/app.router.module.ts
+++ routing-start/src/app/app.router.module.ts
@@ -6,6 +6,7 @@ import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { CanDeactivateGuard } from "./servers/edit-server/can-deactive-guard.service";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
+import { ServerResolver } from "./servers/server/server.resolver";
 import { ServerComponent } from "./servers/server/server.component";
 import { ServersComponent } from "./servers/servers.component";
 import { UserComponent } from "./users/user/user.component";
@@ -19,22 +20,25 @@ const appRoutes: Routes = [
     ]
   },
   {
-    path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-      { path: ':id', component: ServerComponent },
+    path: 'servers',
+    canActivateChild: [AuthGuard],
+    component: ServersComponent,
+    children: [
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
       { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
     ]
   },
   // {path: 'not-found', component: PageNotFoundComponent},
-  {path: 'not-found', component: ErrorPageComponent, data: {message: 'Página não encontrada!'}},
-  {path: '**', redirectTo: 'not-found'},
+  { path: 'not-found', component: ErrorPageComponent, data: { message: 'Página não encontrada!' } },
+  { path: '**', redirectTo: 'not-found' },
 ]
 
 @NgModule({
-  imports:[
+  imports: [
     RouterModule.forRoot(appRoutes)
   ],
-  exports:[RouterModule]
+  exports: [RouterModule]
 })
-export class AppRouterModule{
+export class AppRouterModule {
 
 }
