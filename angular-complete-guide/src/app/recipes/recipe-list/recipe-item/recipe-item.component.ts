e59f303ec80bef24c3194576e0c0262b21cf1ba2
Ulisses Hen <ulisseshen@gmail.com>
2021-11-09 16:34:31 -0300

    adicionado navegacao ao itens do recipe

--- angular-complete-guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ angular-complete-guide/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -10,14 +10,8 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe!: Recipe;
-
-  constructor(private recipeService:RecipeService) { }
+  @Input() index!: number;
 
   ngOnInit(): void {
   }
-
-  onSelected(){
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
-
 }
