ae8569f877aa5e807ac8e053a912884178088842
Ulisses Hen <ulisseshen@gmail.com>
2021-11-09 20:31:27 -0300

    adicionado navegacao para o componente edit-recipe

--- angular-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ angular-complete-guide/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -13,16 +13,21 @@ export class RecipeDetailComponent implements OnInit {
   recipe!: Recipe;
   id!: number;
   constructor(private recipeService: RecipeService,
+    private router: Router,
     private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.params
-    .subscribe(
-      (params:Params)=>{
-        this.id = +params['id'];
-        this.recipe = this.recipeService.getRecipe(this.id);
-      }
-    );
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipeService.getRecipe(this.id);
+        }
+      );
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route })
   }
 
   onAddToShoppingList() {
--- angular-complete-guide/src/app/router/app-routing/app-routing.module.ts
+++ angular-complete-guide/src/app/router/app-routing/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RecipeDetailComponent } from 'src/app/recipes/recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from 'src/app/recipes/recipe-edit/recipe-edit.component';
 import { RecipeStartComponent } from 'src/app/recipes/recipe-start/recipe-start.component';
 import { RecipesComponent } from 'src/app/recipes/recipes.component';
 import { ShoppingListComponent } from 'src/app/shopping-list/shopping-list.component';
@@ -9,7 +10,9 @@ const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   { path: 'recipes', component: RecipesComponent, children:[
     { path: '', component:RecipeStartComponent},
-    { path: ':id', component:RecipeDetailComponent}
+    { path: 'new', component:RecipeEditComponent},
+    { path: ':id', component:RecipeDetailComponent},
+    { path: ':id/edit', component:RecipeEditComponent}
   ] },
   { path: 'shopping-list', component: ShoppingListComponent },
 ]
