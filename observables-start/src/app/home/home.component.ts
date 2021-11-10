a0be0f33ddfec5b0d6286d20296e13604e5b9e7a
Ulisses Hen <ulisseshen@gmail.com>
2021-11-10 16:38:17 -0300

    testando interval opertor do rxjs para compreender o funcionamento dos Observables

--- observables-start/src/app/home/home.component.ts
+++ observables-start/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  subscription:Subscription;
   constructor() { }
 
   ngOnInit() {
+   this.subscription =  interval(1000)
+      .subscribe(count => console.log(count));
   }
 
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
