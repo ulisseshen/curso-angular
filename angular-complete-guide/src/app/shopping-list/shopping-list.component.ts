378df0748eb2d6a7bf2bc420ae5f07b6bd6208ad
Ulisses Hen <ulisseshen@gmail.com>
2021-11-10 19:26:15 -0300

    adicionado Subscription nos ingredients

--- angular-complete-guide/src/app/shopping-list/shopping-list.component.ts
+++ angular-complete-guide/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
@@ -8,17 +9,23 @@ import { ShoppingListService } from './shopping-list.service';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients!:Ingredient[];
+  ingredientsSubscription!:Subscription;
   constructor(private slService:ShoppingListService) { }
+
 
   ngOnInit(): void {
     this.ingredients = this.slService.getIngredients();
-    this.slService.ingredientsChanged
+    this.ingredientsSubscription = this.slService.ingredientsChanged
     .subscribe(
       (ingredients)=>{
         this.ingredients = ingredients;
       }
     )
+  }
+
+  ngOnDestroy(): void {
+    this.ingredientsSubscription.unsubscribe();
   }
 }
