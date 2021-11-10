cf6e7bb6fe2908e72fb377689524c7e127fc1abb
Ulisses Hen <ulisseshen@gmail.com>
2021-11-10 18:59:59 -0300

    feito o uso de Sujbect para comunicação cruzada de componentes

--- observables-start/src/app/app.component.ts
+++ observables-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  constructor() {}
+  userAtivado = false;
+  constructor(private userService: UserService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.userService.activatedEmmitter
+    .subscribe((didActivate) =>{
+      this.userAtivado = didActivate;
+    })
+  }
 }
