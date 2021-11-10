0938cdac5f8c1c9ab6efa3caf80764f1247fe641
Ulisses Hen <ulisseshen@gmail.com>
2021-11-10 19:24:26 -0300

    utilizado Subject em lugar de EventEmitter

--- angular-complete-guide/src/app/recipes/recipes.component.ts
+++ angular-complete-guide/src/app/recipes/recipes.component.ts
@@ -1,25 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from './recipe.model';
-import { RecipeService } from './recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
-  providers:[RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  recipeSelected!:Recipe;
-
-  constructor(private recipeService:RecipeService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipeService.recipeSelected
-    .subscribe(
-      (recipe:Recipe) =>{
-        this.recipeSelected = recipe;
-      }
-    )
+
   }
 
 }
--- angular-complete-guide/src/app/shopping-list/shopping-list.service.ts
+++ angular-complete-guide/src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
-import { EventEmitter } from "@angular/core";
+import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService {
-  ingredientsChanged = new EventEmitter<Ingredient[]>();
+  ingredientsChanged = new Subject<Ingredient[]>();
   ingredients:Ingredient[] = [
     new Ingredient("Tomate", 5),
     new Ingredient("Maçã",3)
@@ -14,11 +14,11 @@ export class ShoppingListService {
 
   addIngredient(ingredient:Ingredient){
     this.ingredients.push(ingredient);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 
   addIngredients(ingredients: Ingredient[]){
     this.ingredients.push(...ingredients);
-    this.ingredientsChanged.emit(this.ingredients.slice());
+    this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
