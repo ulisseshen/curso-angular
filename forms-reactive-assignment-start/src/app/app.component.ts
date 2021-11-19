1c6e85e0548e7c9a3ba119da60cd19601e052dee
Ulisses Hen <ulisseshen@gmail.com>
2021-11-19 19:15:14 -0300

    desafio terminado

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,76 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatuses = ['Estável', 'Crítico', 'Finalizado']
+  projectForm: FormGroup;
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [Validators.required, this.forbiddenName]),
+      email: new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmail),
+      status: new FormControl('stable'),
+    })
+  }
+
+  onSubmit() {
+    console.log(this.projectForm.value)
+  }
+
+  get projectNameIsEmpty() {
+    return this.isControlInvalidWithError('projectName', 'required');
+  }
+
+  get projectNameIsForbidden() {
+    return this.isControlInvalidWithError('projectName', 'forbiddenName')
+  }
+
+  get emailIsEmpty() {
+    return this.isControlInvalidWithError('email', 'required');
+  }
+
+  get emailIsInvalid() {
+    return this.isControlInvalidWithError('email', 'email');
+  }
+
+  get emailIsForbidden() {
+    return this.isControlInvalidWithError('email', 'emailIsForbidden');
+  }
+
+  private isControlInvalidWithError(controlName: string, errorName: string) {
+    return !this.projectForm.get(controlName).valid
+      && this.projectForm.get(controlName).touched
+      && this.projectForm.get(controlName).errors
+      && this.projectForm.get(controlName).errors[errorName];
+  }
+
+  forbiddenName(control: FormControl): { [s: string]: boolean } {
+    if ('Test' === control.value) {
+      return { 'forbiddenName': true }
+    }
+    return null;
+  }
+
+  forbiddenEmail(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'ulisses@email.com') {
+          resolve({ 'emailIsForbidden': true })
+        } else {
+          resolve(null);
+        }
+      }, 700)
+    });
+
+    return promise;
+  }
+
+
+
 }
